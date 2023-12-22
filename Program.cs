0051e4afa55f189bae05a97fb9b6e5421c61ca24
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2023-12-22 16:36:22 -0300

    Desafio Finalizado!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("555-555", "C6", "i12345663123", 32);
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Facebook");
+
+Console.WriteLine("----------------------------------------");
+
+Iphone i1 = new Iphone("333-333", "11", "c123453m12354", 128);
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Instagram");
